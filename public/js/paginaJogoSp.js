6f55e441110b35bf84dfa396617a204f750232ac
Sara Freitas <saratavaresfre@gmail.com>
2026-04-04 21:58:12 +0100

    implementação dos desafios para o modo de jogo Singleplayer

--- public/js/paginaJogoSp.js
+++ public/js/paginaJogoSp.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const timer = localStorage.getItem("timer");
     const finalizarBtn = document.getElementById("finalizar-jogo");
     const display = document.getElementById("timer-display");
+    const challengeType = localStorage.getItem("challengeType");
+    const challengeValue = localStorage.getItem("challengeValue");
+
+    let palavrasValidas = 0;
 
     if (timer === "none") {
         finalizarBtn.style.display = "block";
@@ -25,12 +29,33 @@ document.addEventListener("DOMContentLoaded", () => {
         if (event.key === "Enter") {
         let palavra = input.value;
 
+    if (validarPalavra(palavra)) {
         adicionarPalavra(palavra);
         adicionarPontos(20);
+        palavrasValidas++;
+    } else {
+        mostrarAlerta("Palavra inválida para este desafio!");
+    }
 
         input.value = "";
         }
     });
+
+    function validarPalavra(palavra) {
+        if (challengeType === "Não") return true;
+
+        const tamanho = palavra.length;
+
+        if (challengeType === "Mín. letras") {
+            return tamanho >= parseInt(challengeValue);
+        }
+
+        if (challengeType === "Máx. letras") {
+            return tamanho <= parseInt(challengeValue);
+        }
+
+        return true;
+    }
 
     function adicionarPalavra(palavra) {
         let lista = document.getElementById("palavras-descobertas");
@@ -65,10 +90,30 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
     function terminarJogo() {
+        if (challengeType === "Objetivo: nº de palavras") {
+            if (palavrasValidas < parseInt(challengeValue)) {
+                mostrarAlerta("Ainda não atingiu o número de palavras necessário!");
+                return;
+            }
+        }
         window.location.href = "fimDeJogo.html";
     }
 
-finalizarBtn.addEventListener("click", terminarJogo);
+    finalizarBtn.addEventListener("click", terminarJogo);
+
+    function mostrarAlerta(mensagem) {
+        const container = document.getElementById("alerta-container");
+
+        container.innerHTML = `
+            <div class="alert alert-success">
+                ${mensagem}
+            </div>
+        `;
+
+        setTimeout(() => {
+            container.innerHTML = "";
+        }, 2000);
+    }
 
 });
 
