3d7c36f656ee326d00dc72cb8baffdb802cdfbea
Mariana <mscodelearning@gmail.com>
2026-04-13 20:31:57 +0100

    inicio da implementacao do frontend da sala privada com css e js para a caixa de texto para o chat e para o botao iniciar jogo

---
+++ public/js/caixaTextoChat.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const input = document.getElementById("mensagemInput");
+const sendBtn = document.getElementById("sendBtn");
+const mensagens = document.getElementById("mensagens");
+
+function enviarMensagem() {
+  const texto = input.value.trim();
+s
+  if (texto === "") return;
+
+  const novaMensagem = document.createElement("div");
+  novaMensagem.classList.add("mensagem");
+  novaMensagem.textContent = texto;
+
+  mensagens.appendChild(novaMensagem);
+  input.value = "";
+  mensagens.scrollTop = mensagens.scrollHeight;
+}
+
+sendBtn.addEventListener("click", enviarMensagem);
+
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    enviarMensagem();
+  }
+});
