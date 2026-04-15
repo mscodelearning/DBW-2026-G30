6dc12c096dfb267106d8e95ccfed9d7acc6d906c
Mariana <mscodelearning@gmail.com>
2026-04-15 09:50:21 +0100

    instalacao do mongodb e mongoose e ligacao a base de dados

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express"; // Framework HTTP usado para criar a aplicação 
 import path from "path"; // Ajuda a construir caminhos de ficheiros de forma segura.
 import { fileURLToPath } from "url"; // Converte a URL do módulo ES num caminho real do sistema.
 
+
+import mongoose from "mongoose";
+import methodOverride from "method-override";
 import homeRoutes from "./routes/homeRoutes.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -12,9 +15,23 @@ app.set("view engine", "ejs"); // Diz ao Express que as views serão renderizada
 app.use(express.static(path.join(__dirname, "public"))); // Expõe CSS, JS e imagens da pasta public.
 app.use(express.urlencoded({ extended: true })); // Converte dados enviados por formulários em req.body.
 
+app.use(methodOverride("_method")); // Permite usar métodos HTTP diferentes dos padrões (como PUT e DELETE).
 
 /** definir o app.use das rotas aqui */
 app.use("/", homeRoutes);
+
+
+mongoose 
+.connect( 
+"mongodb+srv://2003marianas_db_user:mlRRh9PBxq49pWDY@dbw2526.zmknrl1.mongodb.net/?appName=DBW2526&retryWrites=true&w=majority " 
+) 
+.then(() => { 
+console.log("Connected to MongoDB"); 
+}) 
+.catch((err) => { 
+console.log(err); 
+});
+
 
 app.listen(3000, (err) => { 
 if (err) 
