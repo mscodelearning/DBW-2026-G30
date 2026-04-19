68ffe9a7c8cae5a77e2f43bbde8cce955f8f8509
Mariana <mscodelearning@gmail.com>
2026-04-19 15:39:39 +0100

    verificacao do numero de caracteres na palavra passe na pagina altera passe e remocao do codigo documentado do header por ja nao ser necessario pois tem o partials

--- public/js/validacaoCamposPassPg.js
+++ public/js/validacaoCamposPassPg.js
@@ -11,13 +11,19 @@ const form = document.getElementById("formAlterarPassword");
     form.addEventListener("submit", function(event) {
       erro.textContent = "";
 
+      if (nova.value.length < 6) {
+        event.preventDefault();
+        erro.textContent = "A nova palavra-passe deve ter pelo menos 6 caracteres.";
+        return;
+      }
+
       if (nova.value !== confirmar.value) {
         event.preventDefault();
         erro.textContent = "A nova palavra-passe e a confirmação não coincidem.";
         return;
       }
 
-      // Se quiseres apenas testar sem servidor:
+      // testar sem servidor
       event.preventDefault();
       alert("Palavra-passe alterada com sucesso!");
       /*window.location.href = "perfil.html";*/
